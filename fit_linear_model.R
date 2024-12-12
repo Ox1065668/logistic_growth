82b1dbfdf473270ed6af226202a77c3416475240
Aiyla Salam <aiyla.salam@balliol.ox.ac.uk>
2024-12-12 12:12:10 +0000

    update

--- fit_linear_model.R
+++ fit_linear_model.R
@@ -13,11 +13,18 @@ data_subset1 <- growth_data %>% filter(t<1500) %>% mutate(N_log = log(N))
 model1 <- lm(N_log ~ t, data_subset1)
 summary(model1)
 
+# From the summary, we can see the intercept of log(N) ~ t gives the value of 6.8941709.
+# We can take the exponential of this value to determine N0, giving us a value of 986.5075.
+
+# We can also see the rate of change with respect to t is 0.010086. This is our value for r.
+
 # Case 2. N(t) = K
 
-# Filter data for t > 2000 to focus on the plateau phase 
-data_subset2 <- growth_data %>% filter(t>2000)
+# Filter data for t > 2500 to focus on the plateau phase 
+data_subset2 <- growth_data %>% filter(t>2500)
 
 # Fitting a linear model: N ~ 1
 model2 <- lm(N ~ 1, data_subset2)
 summary(model2)
+
+# From the summary of this model, we can see the estimate intercept is 6e+10. This is our value for k. 
